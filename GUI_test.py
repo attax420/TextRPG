7d238c2a1225a9cdb2f487a7ca9fc740290961aa
attax420 <ktaanthrax@gmail.com>
2021-10-14 15:17:15 +0200

    Update GUI_test.py
    Gui now shows the map and has move buttons which move the player and update the map.

--- GUI_test.py
+++ GUI_test.py
@@ -1,83 +1,168 @@
+import threading
 from tkinter import *
 from classes import *
+from threading import *
+
 
 p = Player()
-p.randomize_map()
-p.position_update()
-p.print_map()
-a=10
-  
-window = Tk()
 
-        # code for creating table
-for i in range(10):
-    print(i)
-    for j in range(10): 
+
+def gamelogic():
+    p.randomize_map()
+
+# GameMap to Grid   
+def thread_window_map():
+    window_map = Tk()
+    window_map_frame = Frame()
+    window_button_frame = Frame()
+    def map_update():
+        p.position_update()
+        p.print_map()
+        for i in range(10):
+            for j in range(10):       
+                symbol = ' '
+                entry = Entry(window_map_frame,bg='black', width=2,
+                                    font=('Arial',15,'bold'))
+                
+                if p.map[i][j] == ' ':
+                    symbol = ' '
+                    entry = Entry(window_map_frame,bg='black', width=2,
+                                    font=('Arial',15,'bold'))
+
+                elif p.map[i][j] == colored(' ', 'green', 'on_white'):
+                    symbol = ' '
+                    entry = Entry(window_map_frame,bg='white', width=2,
+                                    font=('Arial',15,'bold'))
+
+                elif p.map[i][j] == colored('X', 'blue'):
+                    symbol = 'X'
+                    entry = Entry(window_map_frame,bg='black',fg='blue', width=2,
+                                    font=('Arial',15,'bold'))
+
+                elif p.map[i][j] == colored('+', 'red'):
+                    symbol = '+'
+                    entry = Entry(window_map_frame,bg='black',fg='red', width=2,
+                                    font=('Arial',15,'bold'))
+
+                elif p.map[i][j] == colored('E', 'green'):
+                    symbol = 'E'
+                    entry = Entry(window_map_frame,bg='black',fg='green', width=2,
+                                    font=('Arial',15,'bold'))
+            
+
+                elif p.map[i][j] == colored('e', 'yellow'):
+                    symbol = 'e'
+                    entry = Entry(window_map_frame,bg='black',fg='yellow', width=2,
+                                    font=('Arial',15,'bold'))
+            
+                elif p.map[i][j] == colored('E', 'yellow'):
+                    symbol = 'E'
+                    entry = Entry(window_map_frame,bg='black',fg='yellow', width=2,
+                                    font=('Arial',15,'bold'))
+
+                elif p.map[i][j] == colored('E', 'red'):
+                    symbol = 'E'
+                    entry = Entry(window_map_frame,bg='black',fg='red', width=2,
+                                    font=('Arial',15,'bold'))
+
+                elif p.map[i][j] == colored('B', 'magenta'):
+                    symbol = 'B'
+                    entry = Entry(window_map_frame,bg='black',fg='magenta', width=2,
+                                    font=('Arial',15,'bold'))                
+
+                entry.grid(row=i, column=j)
+                entry.insert(END, symbol)
+    map_update()
+
+    sword_logo = PhotoImage(file='icon_small.png')
+
+    def move_north():
+        p.move_north()        
+        map_update()
+
+    def move_south():
+        p.move_south()        
+        map_update()
         
-        symbol = ' '
-        e = Entry(window,bg='black', width=2,
-                            font=('Arial',15,'bold'))
-        
-        if p.map[j][i] == ' ':
-            symbol = ' '
-            e = Entry(window,bg='black', width=2,
-                            font=('Arial',15,'bold'))
+    def move_east():
+        p.move_east()        
+        map_update()
 
-        elif p.map[j][i] == colored(' ','green','on_white'):
-            symbol = ' '
-            e = Entry(window,bg='white', width=2,
-                            font=('Arial',15,'bold'))
+    def move_west():
+        p.move_west()
+        map_update()
+    button_go_north = Button(window_button_frame,
+                text='go north',
+                command=move_north,
+                font=('Arial', 10,),
+                fg='white',
+                bg='#000000',
+                relief=SUNKEN,
+                #activeforeground='black',
+                #activebackground='white',
+                state=None,   # DISABLED
+                image=sword_logo,
+                compound='top'
+                )
+    button_go_north.pack(side=LEFT)
 
-        elif p.map[j][i] == colored('X', 'blue'):
-            symbol = 'X'
-            e = Entry(window,bg='black',fg='blue', width=2,
-                            font=('Arial',15,'bold'))
+    button_go_south = Button(window_button_frame,
+                text='go south',
+                command=move_south,
+                font=('Arial', 10,),
+                fg='white',
+                bg='#000000',
+                relief=SUNKEN,
+                #activeforeground='black',
+                #activebackground='white',
+                state=None,   # DISABLED
+                image=sword_logo,
+                compound='top'
+                )
+    button_go_south.pack(side=LEFT)
 
-        elif p.map[j][i] == colored('+', 'red'):
-            symbol = '+'
-            e = Entry(window,bg='black',fg='red', width=2,
-                            font=('Arial',15,'bold'))
+    button_go_east = Button(window_button_frame,
+                text='go east',
+                command=move_east,
+                font=('Arial', 10,),
+                fg='white',
+                bg='#000000',
+                relief=SUNKEN,
+                #activeforeground='black',
+                #activebackground='white',
+                state=None,   # DISABLED
+                image=sword_logo,
+                compound='top'
+                )
+    button_go_east.pack(side=LEFT)
 
-        elif p.map[j][i] == colored('E', 'green'):
-            symbol = 'E'
-            e = Entry(window,bg='black',fg='green', width=2,
-                            font=('Arial',15,'bold'))
+    button_go_west = Button(window_button_frame,
+                text='go west',
+                command=move_west,
+                font=('Arial', 10,),
+                fg='white',
+                bg='#000000',
+                relief=SUNKEN,
+                #activeforeground='black',
+                #activebackground='white',
+                state=None,   # DISABLED
+                image=sword_logo,
+                compound='top'
+                )
+    button_go_west.pack(side=LEFT)
+
+    window_map_frame.pack()
+    window_button_frame.pack()
+    window_map.configure(bg='black')
+    window_map.mainloop()
+
+
+while True:
+
+    thread_gamelogic = threading.Thread(target=gamelogic())
+    thread_map = threading.Thread(target=thread_window_map())
     
 
-        elif p.map[j][i] == colored('e', 'yellow'):
-            symbol = 'e'
-            e = Entry(window,bg='black',fg='yellow', width=2,
-                            font=('Arial',15,'bold'))
+    thread_gamelogic.start
+    thread_map.start
     
-        elif p.map[j][i] == colored('E', 'yellow'):
-            symbol = 'E'
-            e = Entry(window,bg='black',fg='yellow', width=2,
-                            font=('Arial',15,'bold'))
-
-        elif p.map[j][i] == colored('E', 'red'):
-            symbol = 'E'
-            e = Entry(window,bg='black',fg='red', width=2,
-                            font=('Arial',15,'bold'))
-
-        elif p.map[j][i] == colored('B', 'magenta'):
-            symbol = 'B'
-            e = Entry(window,bg='black',fg='magenta', width=2,
-                            font=('Arial',15,'bold'))                
-
-        e.grid(row=i, column=j)
-        e.insert(END, symbol)
-
-
-
- 
-# find total number of rows and
-# columns in list
-
- 
-# create window window S
-
- 
-##window.attributes("-fullscreen",True)
-window.configure(bg='black')
- 
-window.mainloop()
